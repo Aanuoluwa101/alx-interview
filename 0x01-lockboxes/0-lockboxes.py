b8dc8550e8f1b6c521761c8bc3da427f8c32933e
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-05 05:32:38 -0700

    changed logic

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""You have n number of locked boxes in front of you.
-   Each box is numbered sequentially from 0 to n - 1 and 
-   each box may contain keys to the other boxes"""
+"""You have n number of locked boxes in front of you. 
+   Each box is numbered sequentially from 0 to n - 1 
+   and each box may contain keys to the other boxes"""
+
 
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened"""
-    keys = {}
-    for box in boxes:
-        for key in box:
-            if key not in keys:
-                keys[key]  = 1
-            else:
-                keys[key] += 1
+    unlocked_boxes = set([0])  
+    all_keys = [0]
     
-    for box_label in range(1, len(boxes)):
-        if box_label not in keys:
-            return False
-    return True
+    while all_keys:
+        current_box = queue.pop(0)
+        box_keys = boxes[current_box]
+        
+        for key in box_keys:
+            if key not in unlocked_boxes:
+                unlocked_boxes.add(key)
+                all_keys.append(key)
+
+    return len(unlocked_boxes) == len(boxes)
