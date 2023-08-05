bbf7a4f92d007aed5646883898f99b76ddc16047
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-05 03:57:03 -0700

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+##!/usr/bin/python3
+"""You have n number of locked boxes in front of you.
+   Each box is numbered sequentially from 0 to n - 1 and 
+   each box may contain keys to the other boxes"""
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    keys = {}
+    for box in boxes:
+        for key in box:
+            if key not in keys:
+                keys[key]  = 1
+            else:
+                keys[key] += 1
+    
+    for box_label in range(1, len(boxes)):
+        if box_label not in keys:
+            return False
+    return True
