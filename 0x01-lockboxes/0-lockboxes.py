852f52a9c67fd60db876b213fd8726feebd1d139
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-06 18:21:04 -0700

    pycodestyle compliance

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""You have n number of locked boxes in front of you. 
-   Each box is numbered sequentially from 0 to n - 1 
+"""You have n number of locked boxes in front of you.
+   Each box is numbered sequentially from 0 to n - 1
    and each box may contain keys to the other boxes"""
 
 
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened"""
-    unlocked_boxes = set([0])  
+    unlocked_boxes = set([0])
     all_keys = [0]
 
     if not boxes:
         return False
-    
+
     while all_keys:
         current_box = all_keys.pop(0)
 
         if current_box < len(boxes):
             box_keys = boxes[current_box]
-            
             for key in box_keys:
                 if key not in unlocked_boxes:
                     unlocked_boxes.add(key)
