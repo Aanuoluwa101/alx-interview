02a27faa0ec736e7b9dde9e5f944890a4a7a94b7
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-09 08:52:31 -0700

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""In a text file, there is a single character H.
+  Your text editor can execute only two operations in this file: Copy All and Paste.
+  Given a number n, write a method that calculates the fewest number of operations
+  needed to result in exactly n H characters in the file"""
+
+
+def minOperations(n):
+    """calculates the fewest number of operations
+       needed to result in exactly n H characters in the file"""
+    if n < 2:
+        return 0
+
+    divisor = 2
+    factors = []
+    temp_n = n
+
+    while temp_n > 1:
+        while temp_n % divisor == 0:
+            temp_n/=divisor
+            factors.append(divisor)
+        divisor+=1
+
+    highest_prime_factor = max(factors)
+    if highest_prime_factor == n:
+        return highest_prime_factor
+    return highest_prime_factor + 1 + ((n // highest_prime_factor) - 1)
+
