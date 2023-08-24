be98a6e2284c5b977586c34eed84ce1ce519859a
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-24 16:07:35 +0000

    requirements

--- 0x03-log_parsing/check_format.py
+++ 0x03-log_parsing/check_format.py
@@ -7,5 +7,6 @@ import re
 
 def check_log_format(log_string):
     """Checks log string's format"""
-    pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] \"GET /projects/260 HTTP/1\.1" \d+ \d+$'
+    pattern = (r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
+               r'\[.*\] "GET /projects/260 HTTP/1\.1" \d+ \d+$')
     return re.match(pattern, log_string) is not None
